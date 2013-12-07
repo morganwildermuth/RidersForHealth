62e12c04b60744ffba9be39b2531b4ff762aa2ed
morganwildermuth <morgan.wildermuth@gmail.com>
2013-12-06 17:03:44 -0800

    readme added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChimeforChange::Application.config.secret_key_base = 'c95318f591880e0e9577b0dd01c55ed17f99a17bc2b943c8f4b1d484b90442207d27053b45f35e3ee3481f0ae55fbf63d1c94f26e592b12f534d8d5294b88fbe'
